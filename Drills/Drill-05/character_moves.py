b0db8990f7d65477301f7d802e36c2a0e71371d8
gic91(Notebook) <gic911@naver.com>
2018-09-17 14:50:21 +0900

    check 01
    시작 함수 구현

---
+++ Drills/Drill-05/character_moves.py
@@ -0,0 +1,42 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+def Move_01():
+    pass
+def Move_02():
+    pass
+def Move_03():
+    pass
+def Move_04():
+    pass
+def Move_05():
+    pass
+def Move_06():
+    pass
+def Move_07():
+    pass
+def Move_08():
+    pass
+def Move_09():
+    pass
+def Move_10():
+    pass
+
+while True:
+    Move_01()
+    Move_02()
+    Move_03()
+    Move_04()
+    Move_05()
+    Move_06()
+    Move_07()
+    Move_08()
+    Move_09()
+    Move_10()
+
+
+close_canvas()
