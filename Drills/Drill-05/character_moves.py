dd56b170742612404a73a70a12349b1f0cecd33a
gic91(Notebook) <gic911@naver.com>
2018-09-17 15:41:35 +0900

    Move_01 재구현
    움직임 구현

--- Drills/Drill-05/character_moves.py
+++ Drills/Drill-05/character_moves.py
@@ -6,22 +6,36 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 def Move_01():
-    x,y=203,535
-    clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(100, 200, 100, 100, x, y)
-    update_canvas()
-    delay(0.5)
-    get_events()
+    x,y=712,349
+    count = 0
+    while count<150:
+         clear_canvas()
+         grass.draw(400, 30)
+         character.clip_draw(100, 200, 100, 100, x, y)
+         update_canvas()
+         divine_x = (x- 203) /50
+         divine_y = (535- y) /50
+         x = x-divine_x
+         y = y+divine_y
+         count+=1
+         delay(0.01)
+         get_events()
 
 def Move_02():
-    x, y = 132, 243
-    clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(100, 200, 100, 100, x, y)
-    update_canvas()
-    delay(0.5)
-    get_events()
+    x, y = 203, 535
+    count = 0
+    while count < 10:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(100, 200, 100, 100, x, y)
+        update_canvas()
+        divine_x = (x - 132) // 10
+        divine_y = (243 - y) // 10
+        x = x - divine_x
+        y = y + divine_y
+        count += 1
+        delay(0.1)
+        get_events()
 
 def Move_03():
     x, y = 535, 470
@@ -93,14 +107,14 @@ def Move_10():
 while True:
     Move_01()
     Move_02()
-    Move_03()
-    Move_04()
-    Move_05()
-    Move_06()
-    Move_07()
-    Move_08()
-    Move_09()
-    Move_10()
+    #Move_03()
+    #Move_04()
+    #Move_05()
+    #Move_06()
+    #Move_07()
+    #Move_08()
+    #Move_09()
+    #Move_10()
 
 
 close_canvas()
