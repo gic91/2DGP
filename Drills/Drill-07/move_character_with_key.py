44532c8b457be4481a2fad7e84471ddd2d0073f1
gic91(Notebook) <gic911@naver.com>
2018-10-02 16:53:22 +0900

    랜덤 점 생성

--- Drills/Drill-07/move_character_with_key.py
+++ Drills/Drill-07/move_character_with_key.py
@@ -1,6 +1,18 @@
 from pico2d import *
+import random
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+def Go_char(p):
+    
+
+def Make_random_point(x,y):
+    size = 20
+    points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
+    n = 1
+
+    while True:
+        Go_char(points[n - 1], points[n])
+        n = (n + 1) % size
 
 
 open_canvas(KPU_WIDTH,KPU_HEIGHT)
@@ -17,8 +29,6 @@ while running:
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     character.clip_draw(frame * 100, move_Frame * 1, 100, 100, x,y)
     update_canvas()
-
-
     frame = (frame + 1) % 8
     x += dir*5
     delay(0.05)
