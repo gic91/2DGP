6e961968c7b50d5aecb58dac2857c0fafcbd4cf3
gic91(Notebook) <gic911@naver.com>
2018-10-02 17:12:54 +0900

    점 배열 완료

--- Drills/Drill-07/move_character_with_key.py
+++ Drills/Drill-07/move_character_with_key.py
@@ -10,25 +10,26 @@ character = load_image('animation_sheet.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+size =20
+point_x = [(random.randint(0, 1000))for i in range(size)]
+point_y = [(random.randint(0, 1000))for i in range(size)]
 frame = 0
 dir =0
 move_Frame = 300
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, move_Frame * 1, 100, 100, x,y)
+    n=0
+    x= point_x[n]
+    y=point_y[n]
+    character.clip_draw(frame * 100, move_Frame * 1, 100, 100,x,y)
     update_canvas()
     frame = (frame + 1) % 8
     size = 20
-    points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
-    n = 1
-
-    while True:
-
-            x = points[n-1][0]
-            y = points[n-1][1]
-
-        
+    
+    n+=1
+    if(n>20):
+        n=0
 
 close_canvas()
 
