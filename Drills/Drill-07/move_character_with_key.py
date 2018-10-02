95b1013d68f3542589ddaeafaa4fbee0c82e2df0
gic91(Notebook) <gic911@naver.com>
2018-10-02 17:04:29 +0900

    커밋 수정

--- Drills/Drill-07/move_character_with_key.py
+++ Drills/Drill-07/move_character_with_key.py
@@ -1,18 +1,7 @@
 from pico2d import *
 import random
-
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
-def Go_char(p):
-    
 
-def Make_random_point(x,y):
-    size = 20
-    points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
-    n = 1
-
-    while True:
-        Go_char(points[n - 1], points[n])
-        n = (n + 1) % size
 
 
 open_canvas(KPU_WIDTH,KPU_HEIGHT)
@@ -30,8 +19,16 @@ while running:
     character.clip_draw(frame * 100, move_Frame * 1, 100, 100, x,y)
     update_canvas()
     frame = (frame + 1) % 8
-    x += dir*5
-    delay(0.05)
+    size = 20
+    points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
+    n = 1
+
+    while True:
+
+            x = points[n-1][0]
+            y = points[n-1][1]
+
+        
 
 close_canvas()
 
