04d94b1de9242a8593be00bfca76b1ba8f20291a
gic91(Notebook) <gic911@naver.com>
2018-11-04 18:11:53 +0900

    영웅 생성

--- 2D_GAME/game_source/stage3.py
+++ 2D_GAME/game_source/stage3.py
@@ -3,11 +3,15 @@ from pico2d import *
 import game_framework
 import random
 import Main_Stage
-
+import stage3_state
 time_time = Main_Stage.time_time
 
 
-
+shape = [0,1,2,1,2,0,1,0,2,1]
+count =10
+Y = []
+for i in range(0, count):
+    Y.append(i * 150)
 class Back:
     def __init__(self):
         self.image = load_image('game_sprite\\stage3.png')
@@ -34,31 +38,57 @@ class Time:
     def draw(self):
         self.font.draw(1060, 670, '%3d' % self.main_time, (255, 0, 0))
 
-
 class Shell:
 
     def __init__(self):
-        self.image = load_image('game_sprite\\shell.png')
-        self.Count =10
-        self.Y=[]
-        self.color=[1,0,0,2,0,1,2,0,1,2]
-        for i in range(0,self.Count):
-            #self.color.append(random.randint(0,2))
-            self.Y.append(i*200)
+        global count
+        self.image = load_image('game_sprite\\stage3_item.png')
         self.start=500
+
+
     def update(self):
         self.start-=10
         if self.start <=0:
             self.start =0
     def draw(self):
-        for j ,i in enumerate(self.Y):
-                if self.color[j] ==0:
-                #R
-                    self.image.clip_draw(0, 0, 198, 200, 630, 100 + i+self.start)
-                if self.color[j]==1:
-                #B
-                    self.image.clip_draw(203, 0, 200, 200, 630, 100 + i+self.start)
-                if self.color[j]==2:
-                #G
-                    self.image.clip_draw(407, 0, 200, 200, 630, 100 + i+self.start)
+        global shape,Y
+        for j, i in enumerate(Y):
+            if shape[j] == 0:  # <-
+                self.image.clip_draw(212, 255, 60, 200, 790, 200+self.start+Y[j])
+            elif shape[j] == 1:  # l
+                self.image.clip_draw(108, 255, 60, 200, 610, 200+self.start++Y[j])
+            elif shape[j] == 2:  # ->
+                self.image.clip_draw(5, 255, 60, 200, 420, 200+self.start+Y[j])
 
+        self.image.clip_draw(6, 150, 40, 75, 700, 600)
+
+class Hero:
+    def __init__(self):
+        self.image = load_image('game_sprite\\stage3_item.png')
+        self.state =0
+
+    def update(self):
+        global X,count,num,shape,plus
+        if stage3_state.key ==1:
+            self.state = 1#<-
+
+
+        elif stage3_state.key ==2:
+            self.state = 2#l
+
+
+        elif stage3_state.key ==3:
+            self.state = 3#->
+
+
+        elif stage3_state.key ==4 or stage3_state.key ==5  or stage3_state.key ==6:
+            self.state = 0
+    def draw(self):
+        if self.state ==0: #<-
+            self.image.clip_draw(6, 150, 40, 75, 600,1560)
+        elif self.state == 1: #l
+            self.image.clip_draw(6, 150, 40, 75, 600,560)
+        elif self.state == 2: #->
+            self.image.clip_draw(6, 150, 40, 75, 600,300)
+        elif self.state == 3:
+            self.image.clip_draw(6, 150, 40, 75,600,430)
--- 2D_GAME/game_source/stage3_state.py
+++ 2D_GAME/game_source/stage3_state.py
@@ -6,10 +6,26 @@ from pico2d import *
 import game_framework
 import stage3_world
 import game_state
+key =0
+from stage3 import Time
+from stage3 import Back
+from stage3 import Shell
+from stage3 import Hero
 name = "Stage_3State"
-
+time =None
+back =None
+shell =None
+hero =None
 def enter():
-    pass
+    global  time,back,shell,hero
+    time = Time()
+    back = Back()
+    shell = Shell()
+    hero  = Hero()
+    stage3_world.add_object(time, 0)
+    stage3_world.add_object(back, 0)
+    stage3_world.add_object(shell, 0)
+    stage3_world.add_object(hero, 0)
 def exit():
     stage3_world.clear()
 
@@ -22,13 +38,23 @@ def resume():
 
 
 def handle_events():
-    global Start_menu
+    global key
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
-                game_framework.pop_state()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_LEFT:  # R
+            key = 1
+        elif event.type == SDL_KEYUP and event.key == SDLK_LEFT:  # R
+            key = 4
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:  # B
+            key = 2
+        elif event.type == SDL_KEYUP and event.key == SDLK_RIGHT:  # B
+            key = 5
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_DOWN:  # G
+            key = 3
+        elif event.type == SDL_KEYUP and event.key == SDLK_DOWN:  # G
+            key = 6
 
 
 
